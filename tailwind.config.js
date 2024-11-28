4346aa8669a2b0255f9e53694486c0aa2d047671
Рябов Сергей Дмитриевич <ryabov-sd@ranepa.ru>
2024-11-28 11:47:02 +0300

    settings tailwind config, fonst, globals.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
+	// Включение темной темы
+	darkMode: ["class"],
+
+	// Указание путей для скарирования классов tailwind
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+
+	// Глобальный префикс для классов tailwind
+	prefix: "",
+
+	theme: {
+		// Настройка контейнера
+		container: {
+			center: true, // Центрирование по горизонтали
+			padding: "16px", // Отступы внутри контейнера
+		},
+
+		// Определение экранных разверов
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px"
+		},
+
+		// Добавление шрифтов
+		fontFamily: {
+			primary: "var(--font-jetbrainsMono)",
+		},
+
+		extend: {
+			// Определение цветов
+			colors: {
+				primary: "#202023",
+				accent: {
+					DEFAULT: "#88ccca",
+					hover: "#7fb5b5",
+				},
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
 };
